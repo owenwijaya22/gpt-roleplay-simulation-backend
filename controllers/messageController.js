5b7d787be8db0fc305cdeae0ad1cfef168ca680d
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-13 15:03:55 +0800

    fixed environment variables config and added proper response handling

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -17,37 +17,34 @@ exports.getAllMessage = async (req, res) => {
   }
 };
 
-exports.addMessage = async (req, res) => {
-  // try {
+try {
   const { to, from, message, timestamp, roomId } = req.body;
-  const data = await Message.create({
-    to: to,
-    from: from,
-    message: message,
-    timestamp: timestamp,
-    roomId: roomId,
-  });
-  await data.save();
-  res.send(data);
+
+  // Since Message.create() also saves the document, no need to call data.save()
+  const data = await Message.create({ to, from, message, timestamp, roomId });
+
   /**
    * Connecting to Python server
    * The answer returned can be transmitted back to the chat client.
    * This message can then be rendered successfully.
    */
-  //   if (data)
-  //     return res.status(200).json({
-  //       status: 'success',
-  //       data: {
-  //         message: 'Chat Added Successfully',
-  //       },
-  //     });
-  //   return res.status(400).json({
-  //     status: 'failed',
-  //     message: 'Chat was not added into the database',
-  //   });
-  // } catch (err) {
-  //   res.status(404).json({
-  //     status: 'failed',
-  //     message: 'Failed to retrieve messages: ' + err.message,
-  //   });
-};
+  if (data) {
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        message: 'Chat Added Successfully',
+      },
+    });
+  } else {
+    return res.status(400).json({
+      status: 'failed',
+      message: 'Chat was not added into the database',
+    });
+  }
+
+} catch (err) {
+  return res.status(404).json({
+    status: 'failed',
+    message: 'Failed to retrieve messages: ' + err.message,
+  });
+}
