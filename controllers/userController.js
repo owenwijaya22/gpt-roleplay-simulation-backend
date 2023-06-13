a89bbf315f7ef62d4dba8def0084ff6b57b00ddc
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-13 16:14:30 +0800

    added temporary user controllers for user models

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const User = require('../models/userModel');
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
