cf90506ae853a6273c3cdaf69d0cd68c11934069
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-15 13:42:07 +0800

    added room routings for the newly added controllers

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -3,12 +3,17 @@ const roomRouter = require('express').Router();
 const {
     createRoom,
     deleteRoom,
-    leaveRoom
+    leaveRoom,
+    getAllRooms,
+    getRoom
   } = require('../controllers/roomController');
   
 // Room Routes for the frontend to create buttons that make requests to the endpoints
+roomRouter.route('/').get(getAllRooms); // Get all rooms (for testing purposes
 roomRouter.route('/room').post(createRoom); // Create a new room
+roomRouter.route('/room/:roomId').get(getRoom); // Get room info
+roomRouter.route('/room/:roomId').delete(getRoom); // Get a room by its ID
 roomRouter.route('/room/:roomId').delete(deleteRoom); // Delete a room by its ID
 roomRouter.route('/room/:roomId/leave').patch(leaveRoom); // Remove the current user from a room
-  
+
 module.exports = roomRouter;
