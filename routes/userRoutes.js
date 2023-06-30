4e1d5954f1112ef8d69f72531bd3802cd52ad3a0
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-30 14:19:25 +0800

    appended routings for user model

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,26 @@
-const {createUser, getAllUsers, getUsers} = require('../controllers/userController');
-const userRouter = require('express').Router();
+import { Router } from 'express';
+import {
+  createUser,
+  getAllUsers,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
 
+const userRouter = Router();
 
+// create a new user
+userRouter.post('/', createUser);
+// fetch all users
 userRouter.get('/', getAllUsers);
+// fetch users by roomId
 userRouter.get('/:roomId', getUsers);
-userRouter.route('/user').post(createUser);
+// fetch a user by ID
+userRouter.get('/:id', getUserById);
+// update a user by ID
+userRouter.put('/:id', updateUser);
+// delete a user by ID
+userRouter.delete('/:id', deleteUser);
 
-module.exports = userRouter;
+export default userRouter;
