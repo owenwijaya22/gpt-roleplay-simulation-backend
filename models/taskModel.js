989d3888e2a372e120021d49e31afb3f314ece47
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-30 14:20:51 +0800

    created task and task template schemas array

---
+++ models/taskModel.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema as _Schema, model } from 'mongoose';
+
+const { Schema } = mongoose;
+
+// represents each individual steps in one task
+const TaskTemplateSchema = new Schema({
+  // The name of the step: "Introduction", "Conclusion", etc.
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+});
+
+// each task contains an array of TaskTemplate objects; a sequence of steps
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: _Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    company: {
+      type: _Schema.Types.ObjectId,
+      required: true,
+      ref: 'Company',
+    },
+    taskType: {
+      type: String,
+      required: true,
+    },
+    templates: [TaskTemplateSchema], // an array of TaskTemplate objects
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Task = model('Task', TaskSchema);
+export const TaskTemplate = model('TaskTemplate', TaskTemplateSchema);
