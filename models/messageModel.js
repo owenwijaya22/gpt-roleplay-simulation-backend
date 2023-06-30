536e47d4dbf11184f9ab64759c92704e508345e3
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-30 14:18:15 +0800

    added response attribute for message schema

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
 
-const messageSchema = new mongoose.Schema({
-  to: {
-    type: String,
-    trim: true,
-    required: [true, 'A message must have a recipient'],
+const messageSchema = new Schema(
+  {
+    message: {
+      type: String,
+      trim: true,
+      required: [true, 'A message must have content'],
+    },
+    response: {
+      type: String,
+      trim: true,
+      required: [true, 'A message must have a reply'],
+    },
+    // will be mongoose.Schema.Types.ObjectId
+    roomId: {
+      type: String,
+      ref: 'Room',
+      required: [true, 'A message must belong to a chat room'],
+    },
   },
-  from: {
-    type: String,
-    trim: true,
-    required: [true, 'A message must have a sender'],
-  },
-  message: {
-    type: String,
-    trim: true,
-    required: [true, 'A message must have content'],
-  },
-  // will be mongoose.Schema.Types.ObjectId
-  roomId: {
-    type: String,
-    ref: 'Room',
-    required: [true, 'A message must belong to a chat room'],
-  },
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = model('Message', messageSchema);
 
-module.exports = Message;
+export default Message;
