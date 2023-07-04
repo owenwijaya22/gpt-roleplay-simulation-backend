b57b95232f5a1ccea2eedc2b7b3c0074f2c8e3a0
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 11:55:05 +0800

    created event models

---
+++ models/eventModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  choice_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Choice',
+    required: [true, 'An event must be related to a choice'],
+  },
+  next_question_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question',
+    required: false,
+  },
+  next_task_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    required: false,
+  },
+  next_clue_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Clue',
+    required: false,
+  },
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+export default Event;
