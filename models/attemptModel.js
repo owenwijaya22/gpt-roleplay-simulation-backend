81297a91381857839c8c614e2332b6fdbd636711
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 11:54:56 +0800

    created attempt model

---
+++ models/attemptModel.js
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+
+const AttemptSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  companyId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Company',
+    required: true
+  },
+  startTime: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  endTime: {
+    type: Date
+  },
+  taskIds: [{
+    taskId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Task',
+      required: true
+    },
+    complete: {
+      type: Boolean,
+      default: false
+    }
+  }]
+});
+
+const Attempt = model('Attempt', AttemptSchema);
+
+export default Attempt;
