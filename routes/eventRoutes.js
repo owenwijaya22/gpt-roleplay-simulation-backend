8d24e21c422507145afc6ff723ea0416341ed5b2
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-10 11:07:03 +0800

    updated event documentation and examples

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -24,9 +24,9 @@ const eventRouter = Router();
  *   "data": {
  *     "event": {
  *       "_id": "60e73bdf5b3c593b2c5b191e",
- *       "choiceId": "60e73bdf5b3c593b2c5b191d",
+ *       "choiceId": "64a3b774f4e449afdd7938b8",
  *       "next_question_id": "60e73bdf5b3c593b2c5b191c",
- *       "next_task_id": "60e73bdf5b3c593b2c5b191b",
+ *       "next_task_id": "64a38f2f863a68a02809c0bc",
  *       "next_clue_id": "60e73bdf5b3c593b2c5b191a"
  *     }
  *   }
@@ -40,21 +40,57 @@ eventRouter.post('/', createEvent);
  * @tags events
  * @param {string} request.body.choiceId.required - Choice ID
  * @return {object} 200 - Success response - application/json
- * @example response - 200 - Example response
+ * @example request - application/json
  * {
- *   "status": "success",
- *   "message": "Event triggered",
- *   "data": [
- *     {
- *       "nextMoveType": "task",
- *       "nextMove": {
- *         "_id": "60e73bdf5b3c593b2c5b191b",
- *         "title": "Next Task Title",
- *         "description": "Next Task Description"
- *       }
- *     }
- *   ]
+ *  "choiceId": "64a3b774f4e449afdd7938b8"
  * }
+ * @example response - 200 - Example response
+ *   {
+ *       "status": "success",
+ *       "message": "Event triggered",
+ *       "data": [
+ *           {
+ *               "nextMoveType": "task",
+ *               "nextMove": {
+ *                   "_id": "64a38f2f863a68a02809c0bc",
+ *                   "title": "Market Research",
+ *                   "description": "Conduct market research to identify new business opportunities",
+ *                   "completed": false,
+ *                   "company": "64a3c5123510c42f08bb4345",
+ *                   "taskType": "Business Development",
+ *                   "templates": [
+ *                       {
+ *                           "name": "Planning",
+ *                           "desc": "Outline the objective, scope, and methods for the research",
+ *                           "order": 1,
+ *                           "_id": "64a38f2f863a68a02809c0bd"
+ *                       },
+ *                       {
+ *                           "name": "Data Collection",
+ *                           "desc": "Collect data from various sources",
+ *                           "order": 2,
+ *                           "_id": "64a38f2f863a68a02809c0c0"
+ *                       },
+ *                       {
+ *                           "name": "Analysis",
+ *                           "desc": "Analyze the data to extract insights",
+ *                           "order": 3,
+ *                           "_id": "64a38f2f863a68a02809c0c3"
+ *                       },
+ *                       {
+ *                           "name": "Reporting",
+ *                           "desc": "Compile the findings into a report",
+ *                           "order": 4,
+ *                           "_id": "64a38f2f863a68a02809c0c6"
+ *                       }
+ *                   ],
+ *                   "createdAt": "2023-07-04T03:17:03.717Z",
+ *                   "updatedAt": "2023-07-04T03:17:03.717Z",
+ *                   "__v": 0
+ *               }
+ *           }
+ *       ]
+ *   }
  */
 eventRouter.get('/', triggerEvent);
 
