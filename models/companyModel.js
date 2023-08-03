a16e9912cd0ee7d7ae49193dff45e492f018710d
Owen Valentinus <owenwijaya89@gmail.com>
2023-08-03 14:43:04 +0800

    updated company model

--- models/companyModel.js
+++ models/companyModel.js
@@ -1,37 +1,50 @@
 import { Schema, model } from 'mongoose';
 
-const companyScheme = new Schema({
-  name: {
+const jobSchema = new Schema({
+  title: {
     type: String,
-    required: [true, 'A company must have a name'],
-    unique: true,
+    required: [true, 'A job must have a title'],
   },
   description: {
     type: String,
-    required: [true, 'A company must have a description'],
+    required: [true, 'A job must have a description'],
   },
-  image: {
+  requirements: {
     type: String,
-    required: [true, 'A company must have an image/logo'],
+    required: [true, 'A job must have requirements'],
   },
   video: {
     type: String,
-    required: [true, 'A company must have a video'],
-  },
-  website: {
-    type: String,
-    required: [true, 'A company must have a website'],
-    unique: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  modifiedAt: {
-    type: Date,
-    default: Date.now(),
+    required: false,
   },
 });
+
+const companyScheme = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A company must have a name'],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'A company must have a description'],
+    },
+    image: {
+      type: String,
+      required: [true, 'A company must have an image/logo'],
+    },
+    website: {
+      type: String,
+      required: [true, 'A company must have a website'],
+      unique: true,
+    },
+    jobs: [jobSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Company = model('Company', companyScheme);
 
