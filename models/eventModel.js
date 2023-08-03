a8de04878f87fc08e9ceffcbae2fca41af1e80c1
Owen Valentinus <owenwijaya89@gmail.com>
2023-08-03 14:43:16 +0800

    updted event models

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,28 +1,27 @@
 import mongoose from 'mongoose';
+import eventEnums from '../contants/eventEnums.js';
 
-const eventSchema = new mongoose.Schema({
+const useEventTypeEnum = new mongoose.Schema({
+  nextEventType: {
+    type: String,
+    enum: eventEnums,
+    required: [true, 'An event must have a type'],
+  },
+});
+
+const userEvent = new mongoose.Schema({
   choiceId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Choice',
     required: [true, 'An event must be related to a choice'],
   },
-  next_question_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Question',
-    required: false,
-  },
-  next_task_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Task',
-    required: false,
-  },
-  next_clue_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Clue',
-    required: false,
+  nextEventType: [useEventTypeEnum],
+  nextEventId: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
   },
 });
 
-const Event = mongoose.model('Event', eventSchema);
+const UserEvent = mongoose.model('UserEvent', userEvent);
 
-export default Event;
+export default UserEvent;
