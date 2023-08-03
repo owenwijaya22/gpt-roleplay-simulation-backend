b7657f0bf9bdebfe6abcb89d5982a414991caf87
Owen Valentinus <owenwijaya89@gmail.com>
2023-08-03 14:43:49 +0800

    updated room model

--- models/roomModel.js
+++ models/roomModel.js
@@ -1,24 +1,36 @@
 import { Schema, model } from 'mongoose';
 
-const roomSchema = new Schema({
-  user: {
-    type: String,
-    ref: 'User',
-    required: true,
-  },
-  prompt: {
-    type: String,
-    required: true,
-  },
-  members: [
-    {
-      // will be mongoose.Schema.Types.ObjectId
-      // keep track of which users are in which room
+const roomSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    prompt: {
       type: String,
       required: true,
     },
-  ],
-});
+    npc: {
+      type: Schema.Types.ObjectId,
+      ref: 'NPC',
+      required: true,
+    },
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: 'Message',
+      default: null
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Rooms = model('Room', roomSchema);
 
