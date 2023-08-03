11a7963818ce96d23e07143755636c9037847fe4
Owen Valentinus <owenwijaya89@gmail.com>
2023-08-03 14:38:11 +0800

    added sockets, helmet, and more routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import expressJSDocSwagger from 'express-jsdoc-swagger';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import helmet from 'helmet';
 
 import messageRouter from './routes/messageRoutes.js';
 import roomRouter from './routes/roomRoutes.js';
@@ -11,9 +14,10 @@ import questionRouter from './routes/questionRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
 import attemptRouter from './routes/attemptRoutes.js';
 import eventRouter from './routes/eventRoutes.js';
+import clueRouter from './routes/clueRoutes.js';
+import proposalRouter from './routes/proposalRoute.js';
+// import hpp from 'hpp';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -38,13 +42,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+// app.use(hpp());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-app.use('/api/message', messageRouter);
+app.use('/api/messages', messageRouter);
 app.use('/api/rooms', roomRouter);
 app.use('/api/users', userRouter);
 app.use('/api/companies', companyRouter);
@@ -53,5 +60,7 @@ app.use('/api/questions', questionRouter);
 app.use('/api/tasks', taskRouter);
 app.use('/api/attempts', attemptRouter);
 app.use('/api/events', eventRouter);
+app.use('/api/clue', clueRouter);
+app.use('/api/proposal', proposalRouter);
 
 export default app;
