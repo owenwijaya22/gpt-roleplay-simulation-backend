31973a2a4c19fc964c57817c08fdb8fce08c92f8
Owen Valentinus <owenwijaya89@gmail.com>
2023-08-03 14:40:20 +0800

    designed event controllers

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,13 +3,61 @@ import Event from '../models/eventModel.js';
 import Question from '../models/questionModel.js';
 import { Task } from '../models/taskModel.js';
 import Clue from '../models/clueModel.js';
+import Attempt from '../models/attemptModel.js';
+
+export const requestHandler = async (req, res) => {
+  try {
+    const { nextEventType } = req.body;
+    if (!nextEventType) {
+      return res.status(400).json({
+        message: 'Missing next event type',
+      });
+    }
+    const data = {};
+    if (nextEventType.includes('NEXT_QUESTION')) {
+      data.question = await getNextQuestion(req.body);
+    }
+    if (nextEventType.includes('NEW_TASK')) {
+      data.question = await getNextQuestion(req.body);
+    }
+    if (nextEventType.includes('UNLOCK_CLUE')) {
+      data.question = await getNextQuestion(req.body);
+    }
+    if (nextEventType.includes('COMPLETE_TASK')) {
+      data.question = await getNextQuestion(req.body);
+    }
+    if (nextEventType.includes('UNLOCK_AI')) {
+      data.question = await getNextQuestion(req.body);
+    }
+    if (Object.keys(data).length === 0) {
+      return res.status(400).json({
+        message: 'Invalid next event type',
+      });
+    }
+    return res.status(200).json({
+      data,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+};
+
+// const getNextQuestion = async (body) => {
+//   const { attemptId, currentQuestion } = body;
+//   const currentAttempt = await Attempt.findById(attemptId);
+//   if (!currentAttempt) {
+//     throw new Error('No attempt found with that ID');
+//   }
+//   const { questions } = currentAttempt.session;
+// };
 
 export const createEvent = async (req, res) => {
   try {
     const { choiceId, next_question_id, next_task_id, next_clue_id } = req.body;
     if (!choiceId && (next_clue_id || next_question_id || next_task_id)) {
       return res.status(400).json({
-        status: 'error',
         message:
           'An event must be related to a choice, and only one of next_clue_id, next_question_id, next_task_id can be specified',
       });
@@ -21,15 +69,11 @@ export const createEvent = async (req, res) => {
       next_clue_id,
     });
     res.status(201).json({
-      status: 'success',
       message: 'Event Created',
-      data: {
-        event: newEvent,
-      },
+      event: newEvent,
     });
   } catch (err) {
     res.status(500).json({
-      status: 'error',
       message: err.message,
     });
   }
@@ -41,7 +85,6 @@ export const triggerEvent = async (req, res) => {
     const event = await Event.findOne({ choiceId });
     if (!event) {
       return res.status(404).json({
-        status: 'error',
         message: 'No event found associated with this choice',
       });
     }
@@ -60,16 +103,15 @@ export const triggerEvent = async (req, res) => {
     if (next_clue_id) {
       const nextMoveType = 'clue';
       const nextMove = await Clue.findById(next_clue_id);
+      await Clue.findByIdAndUpdate(next_clue_id, { locked: false });
       data.push({ nextMoveType, nextMove });
     }
     return res.status(200).json({
-      status: 'success',
       message: 'Event triggered',
       data,
     });
   } catch (err) {
     res.status(500).json({
-      status: 'error',
       message: err.message,
     });
   }
